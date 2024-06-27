96d6aa939ac8cef836c7cea9594ec105ca893473
Brian Jackson <brianjackson1224@gmail.com>
2024-06-26 19:52:32 -0700

    Project Initialization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
